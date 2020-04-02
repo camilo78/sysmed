b91fb29e063a9932426cdac28e29a41246cd91a8
Camilo Alvarado <camilo.alvarado0501@gmail.com>
2020-04-02 15:15:10 -0600

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="{{ asset('/favicon.png') }}" />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
